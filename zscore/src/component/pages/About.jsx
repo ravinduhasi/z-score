0afd4dedfe37c80c7a583464a9b8c4f9875900b4
Ravindu Hasith <ravinduhasithict@gmail.com>
2024-10-02 11:14:49 +0530

    create about page

--- zscore/src/component/pages/About.jsx
+++ zscore/src/component/pages/About.jsx
@@ -1,8 +1,36 @@
-// About.js
 import React from 'react';
+import Images2 from '../images/coverImg.jpg'
+import '../styles/about.css';
 
-function About() {
-  return <h2>About Page</h2>;
+const About = () => {
+  return (
+    <div>
+       
+       <div className="main">
+            <div className="imgbox">
+                <img src={Images2} alt="" srcset="" className='imegs2' />
+            </div>
+            <div className="textbox">
+               <h2>About us </h2>
+               <div className="downtext">
+                  <p>We are a small software engineering team <br/>in Sri Lanka. 
+                    We are pioneers in finding new solutions<br/> to real world 
+                    problems using new technologies and knowledge.</p>
+
+                    <h3>Heare is our team</h3>
+                    <div className="carts">
+                       <div className="cartbox">
+                           
+                       </div>
+                       <div className="cartbox">
+                           
+                       </div>
+                    </div>
+               </div>
+            </div>
+       </div>
+    </div>
+  )
 }
 
-export default About;
+export default About
