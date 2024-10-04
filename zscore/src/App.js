010c27c32e3f7cc78c9e63c932344dcc5ae5cd6e
ravinduhasi <ravinduhasithict@gmail.com>
2024-10-04 17:45:40 +0530

    test2

--- zscore/src/App.js
+++ zscore/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} exact /> {/* Exact match for Home */}
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Conct />} />
+        <Route path="/contact" element={<Contact />} />
         
       </Routes>
     </BrowserRouter>
